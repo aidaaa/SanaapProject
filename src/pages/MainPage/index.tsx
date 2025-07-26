f51d49fb1d1b57296b88630adee8c762723e3e56
aidaaa <a.mosayebi478@gmail.com>
2025-07-26 20:37:25 +0330

    change layout to MainPage

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import LogoIcon from "../../images/logo.svg";
+import "./style.css";
+import PhoneEntry from "../PhoneEntry";
+import CodeValidate from "../CodeValidate";
+import UserPersonalData from "../UserPersonalData";
+import UserPersonalDetailsData from "../UserPersonalDetailsData";
+import UserStatus from "../UserStatus";
+
+enum UserValidateStep {
+  PhoneNumber,
+  SentCode,
+  PersonalData,
+  PersonalDetailsData,
+  UserStatus,
+}
+
+const MainPage = () => {
+  const [validationStep, setValidationStep] = useState<UserValidateStep>(
+    UserValidateStep.PersonalDetailsData
+  );
+
+  const validatePhoneSuccess = useCallback((step: UserValidateStep) => {
+    setValidationStep(step);
+  }, []);
+
+  return (
+    <div className="layout-container">
+      <div className="layout-header">
+        <img src={LogoIcon} />
+      </div>
+
+      {validationStep === UserValidateStep.PhoneNumber && (
+        <PhoneEntry
+          onSuccess={() => validatePhoneSuccess(UserValidateStep.SentCode)}
+        />
+      )}
+      {validationStep === UserValidateStep.SentCode && (
+        <CodeValidate
+          onSuccess={() => validatePhoneSuccess(UserValidateStep.PersonalData)}
+        />
+      )}
+      {validationStep === UserValidateStep.PersonalData && (
+        <UserPersonalData
+          onSuccess={() =>
+            validatePhoneSuccess(UserValidateStep.PersonalDetailsData)
+          }
+        />
+      )}
+
+      {validationStep === UserValidateStep.PersonalDetailsData && (
+        <UserPersonalDetailsData
+          onSuccess={() => validatePhoneSuccess(UserValidateStep.UserStatus)}
+        />
+      )}
+      {validationStep === UserValidateStep.UserStatus && <UserStatus />}
+    </div>
+  );
+};
+
+export default MainPage;
